958af1ce861c5b6eddbfcfd6ec10f8c2f241fca8
lukesalamone <lukesalamone94@gmail.com>
2020-12-16 19:25:21 -0600

    achieved 97% accuracy

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from run_model import RunModel as run
+from run_model import RunModel
 
 
 
-
-
+welcome =   """
+            Welcome to the interactive digit classifier. We will use a convolutional neural network to classify 
+            handwritten digits into one of 10 categories.
+            """
+runner = RunModel()
 
 if __name__ == "__main__":
-    print('Welcome to the interactive digit classifier. We will use a convolutional neural network to classify handwritten digits into one of 10 categories.')
+    print(welcome)
+    runner.load()
 
-    epochs = input('Number of epochs to train for (default 100): ')
+    epochs = input('Number of epochs to train for (default is 2): ')
+    epochs = 2 if not epochs else int(epochs)
+    print('Training for %d epochs' % (epochs))
+
+    model = runner.train(epochs)
+    avg_loss, accuracy = runner.test(model, runner.testloader)
+    print('accuracy: ', accuracy)
--- run_model.py
+++ run_model.py
@@ -1,6 +1,12 @@
-import mnist
+import torch.optim as optim
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 
-
+from digit_classifier import Digit_Classifier
 
 class RunModel():
 
@@ -8,31 +14,102 @@ class RunModel():
         self.loaded = False
         self.trained = False
 
-    def load():
-        print('loading images...')
-        self.train_images = mnist.train_images()
-        self.test_images = mnist.test_images()
+    def load(self):
+        transform = transforms.Compose(
+            [transforms.ToTensor(),
+             transforms.Normalize(0.5, 0.5)])
 
-        print('loading labels...')
-        self.train_labels = mnist.train_labels()
-        self.test_labels = mnist.test_labels()
+        trainset = torchvision.datasets.MNIST(root='./data', train=True,
+                                              download=True, transform=transform)
+        self.trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+        testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+        self.testloader = DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
 
-        print('loading complete')
-        self.loaded = True
+    def train(self, epochs=100):
+        criterion = nn.CrossEntropyLoss()
+        model = Digit_Classifier()
+        optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        self.training_loss = []
 
+        for epoch in range(epochs):  # loop over the dataset multiple times
 
-    def train(epochs=100):
+            running_loss = 0.0
+            for i, data in enumerate(self.trainloader, 0):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+                if i % 2000 == 1999:    # print every 2000 mini-batches
+                    self.training_loss.append(running_loss / 2000)
+                    print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+                    running_loss = 0.0
+
+        print('Finished Training')
         self.trained = True
+        self.model = model
+        return model
 
-    def test():
-        pass
+    def test(self, model, dataloader):
+        criterion = nn.CrossEntropyLoss()
+        test_loss = []
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in dataloader:
+                images, labels = data
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                test_loss.append(criterion(outputs, labels.long()).item())
 
-    def show_metrics():
+        self.avg_loss = np.mean(test_loss)
+        self.accuracy = (100 * correct / total)
+        return self.avg_loss, self.accuracy
+
+    def show_metrics(self):
         if not self.trained:
             print('model has not been trained yet!')
             return
 
-    def show_trained_layers():
+        print('Average')
+
+    def show_trained_layers(self):
         if not self.trained:
             print('model has not been trained yet!')
             return
+
+
+    def train_one(self, model, data_loader, optimizer, device=torch.device('cpu')):
+        criterion = nn.CrossEntropyLoss()
+        train_loss = []
+        correct = 0
+        total = 0
+
+        for batch, labels in data_loader:
+            batch, labels = batch.to(device), labels.to(device)
+            optimizer.zero_grad()
+
+            output = model(batch.float())
+            loss = criterion(output, labels.long())
+            loss.backward()
+            optimizer.step()
+
+            train_loss.append(loss.item())
+            predicted = torch.max(output.data, 1)[1]
+
+            for i in range(len(predicted)):
+                correct += (1 if predicted[i] == labels[i] else 0)
+                total += 1
+
+        return model, np.mean(train_loss), (100 * correct / total)
